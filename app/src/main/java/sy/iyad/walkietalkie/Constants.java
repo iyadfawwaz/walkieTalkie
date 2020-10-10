819da80299429aa4c41da4a2ade3772f803c0fb4
SyriaLink <se.black3d@gmail.com>
2020-10-10 15:49:24 +0300

    new

---
+++ app/src/main/java/sy/iyad/walkietalkie/Constants.java
@@ -0,0 +1,8 @@
+package sy.iyad.walkietalkie;
+
+/** A set of constants used within the app. */
+public class Constants {
+    /** A tag for logging. Use 'adb logcat -s WalkieTalkie' to follow the logs. */
+    public static final String TAG = "WalkieTalkie";
+}
+
